035d696460b3eb957f7c10e9bc089280b873c8dd
RogueNPC <jordanbtansay@gmail.com>
2022-02-01 11:11:51 -0800

    Update calculate_grades.py

--- exercises/composing-methods-refactor/calculate_grades.py
+++ exercises/composing-methods-refactor/calculate_grades.py
@@ -2,27 +2,37 @@
 # Example for Compose Methods: Extract Method.
 import math
 
-def print_stat():
+def generate_grades():
+    # Generates grades from user input
     grade_list = []
-    # Get the inputs from the user
-    n_student = 5
-    for _ in range(n_student):
+    students = 5
+    for i in range(students):
         grade_list.append(int(input('Enter a number: ')))
+    return grade_list
 
-    # Calculate the mean and standard deviation of the grades
-    total = 0 # Do you think 'sum' is a good var name? Run pylint to figure out!
+def calculate_mean():
+    # Calls helper function above
+    grade_list = generate_grades()
+
+    # Calculates the mean of the grades
+    grade_total = 0
     for grade in grade_list:
-        total += grade
-    mean = total / len(grade_list)
-    sd = 0 # standard deviation
+        grade_total += grade
+    mean = grade_total / len(grade_list)
+
+    # Calculates the standard deviation of the grades
+    std_dev = 0
     sum_of_sqrs = 0
     for grade in grade_list:
         sum_of_sqrs += (grade - mean) ** 2
-    sd = math.sqrt(sum_of_sqrs / len(grade_list))
-    # print out the mean and standard deviation in a nice format.
-    print('****** Grade Statistics ******')
-    print("The grades's mean is:", mean)
-    print('The population standard deviation of grades is: ', round(sd, 3))
-    print('****** END ******')
+    std_dev = math.sqrt(sum_of_sqrs / len(grade_list))
 
-print_stat()
+    return mean, std_dev
+
+def print_grade_stats():
+    mean, std_dev = calculate_mean()
+    # print out the mean and standard deviation in a nice format.
+    print("The grades's mean is:", mean)
+    print('The population standard deviation of grades is: ', round(std_dev, 3))
+
+print_grade_stats()
