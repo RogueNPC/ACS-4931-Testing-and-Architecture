280a639e6311e818e28a810fe7c4abb75812c922
RogueNPC <jordanbtansay@gmail.com>
2022-02-03 12:05:27 -0800

    Update best_students.py

--- exercises/composing-methods-refactor/best_students.py
+++ exercises/composing-methods-refactor/best_students.py
@@ -7,6 +7,7 @@
 class Employer:
     def __init__(self, name):
         self.name = name
+
     def send(self, students):
         print("Students' contact info were sent to", self.name + '.')
 
@@ -14,30 +15,52 @@ class Student:
     def __init__(self, gpa, name):
         self.gpa = gpa
         self.name = name
+
     def get_gpa(self):
         return self.gpa
+
     def send_congrat_email(self):
         print("Congrats", self.name + ". You graduated successfully!")
 
 class School:
     def __init__(self, students) -> None:
         self.students = students
+
     def process_graduation(self):
+        # Find the students in the school who have successfully graduated.
+        graduates = self.find_graduates()
+
+        # print student's name who graduated.
+        self.print_graduates(graduates)
+
+        # Send congrat emails to them.
+        self.send_graduate_email(graduates)
+
+        # Find the top 10% of them and send their contact to the top employers
+        self.send_contract_to_employers(graduates)
+
+    def find_graduates(self):
         # Find the students in the school who have successfully graduated.
         min_gpa = 2.5 # minimum acceptable GPA
         passed_students = []
         for s in self.students:
             if s.get_gpa() > min_gpa:
                 passed_students.append(s)
+        return passed_students
 
+    def print_graduates(self, passed_students):
         # print student's name who graduated.
         print('*** Student who graduated *** ')
         for s in passed_students:
             print(s.name)
         print('****************************')
+
+    def send_graduate_email(self, passed_students):
         # Send congrat emails to them.
         for s in passed_students:
             s.send_congrat_email()
+
+    def send_contract_to_employers(self, passed_students):
         # Find the top 10% of them and send their contact to the top employers
         passed_students.sort(key=lambda s: s.get_gpa())
         percentile = 0.9
