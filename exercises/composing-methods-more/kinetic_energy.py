524b9e60333a5cab3979035b3a101c0badb45312
RogueNPC <jordanbtansay@gmail.com>
2022-02-03 10:56:34 -0800

    removed assignment to parameter

--- exercises/composing-methods-more/kinetic_energy.py
+++ exercises/composing-methods-more/kinetic_energy.py
@@ -16,22 +16,22 @@ def calculate_kinetic_energy(mass, distance, time):
         if distance.dis_unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
             # convert from light-year to km unit
             in_km = distance.dis_value * 9.461e12
-            distance = Distance(in_km, "km")
+            in_km_distance = Distance(in_km, "km")
         else:
             print ("unit is Unknown")
             return
 
-    km_per_sec = distance.value/time # [km per sec]
+    km_per_sec = in_km_distance.dis_value/time # [km per sec]
     if mass.mass_unit != 'kg':
         if mass.mass_unit == "solar-mass":
             # convert from solar mass to kg
             kg = mass.mass_value * 1.98892e30 # [kg]
-            mass = Mass(kg, 'kg')
+            kg_mass = Mass(kg, 'kg')
         else:
             print ("unit is Unknown")
             return
 
-    kinetic_energy = 0.5 * mass.value * km_per_sec ** 2
+    kinetic_energy = 0.5 * kg_mass.mass_value * km_per_sec ** 2
     return kinetic_energy
 
 mass = Mass(2, "solar-mass")
