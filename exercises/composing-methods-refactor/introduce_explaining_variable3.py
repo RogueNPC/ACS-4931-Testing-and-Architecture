228f831861f5f55eded1b78f33163d6093cad37a
RogueNPC <jordanbtansay@gmail.com>
2022-02-03 10:13:17 -0800

    Update introduce_explaining_variable3.py

--- exercises/composing-methods-refactor/introduce_explaining_variable3.py
+++ exercises/composing-methods-refactor/introduce_explaining_variable3.py
@@ -4,9 +4,11 @@ WELL_DONE = 900000
 MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
-def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
+def is_cooking_correctly(time, temperature, pressure, desired_state):
+    cook_state = time * temperature * pressure * COOKED_CONSTANT
+
+    if desired_state == 'well-done' and cook_state >= WELL_DONE: 
         return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
+    if desired_state == 'medium' and cook_state >= MEDIUM:
         return True
     return False
