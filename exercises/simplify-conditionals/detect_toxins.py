a457fa52f1ffb973723061f5c5b2426a9929e330
RogueNPC <jordanbtansay@gmail.com>
2022-02-08 10:28:26 -0800

    Update detect_toxins.py

--- exercises/simplify-conditionals/detect_toxins.py
+++ exercises/simplify-conditionals/detect_toxins.py
@@ -3,19 +3,25 @@
 # methods from the condition, then part, and else part(s).
 
 def make_alert_sound():
-    print('made alert sound.')
-def make_accept_sound():
-    print('made acceptance sound')
-
-ingredients = ['sodium benzoate']
-if 'sodium nitrate' in ingredients or 'sodium benzoate' in ingredients\
-or 'sodium oxide' in ingredients:
     print('!!!')
     print('there is a toxin in the food!')    
     print('!!!')
-    make_alert_sound()
-else:
+    print('made alert sound.')
+
+def make_accept_sound():
     print('***')
     print('Toxin Free')
     print('***')
+    print('made acceptance sound')
+
+def toxic():
+    return ('sodium nitrate' in ingredients 
+            or 'sodium benzoate' in ingredients 
+            or 'sodium oxide' in ingredients)
+
+
+ingredients = ['sodium benzoate']
+if toxic():
+    make_alert_sound()
+else:
     make_accept_sound()
