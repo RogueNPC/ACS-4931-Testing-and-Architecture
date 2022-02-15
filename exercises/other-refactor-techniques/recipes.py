211dfa39b53f820f3b703339beb08bcf2ef8237e
RogueNPC <jordanbtansay@gmail.com>
2022-02-15 10:09:52 -0800

    Update recipes.py

--- exercises/other-refactor-techniques/recipes.py
+++ exercises/other-refactor-techniques/recipes.py
@@ -12,28 +12,31 @@ class FoodInfo:
         self.ingredients = ingredients
         self.recipie = recipe
 
-butternut_sqaush_soup = FoodInfo('butternut squash soup', 45, True, 'soup', 'North African', 
-                                ['butter squash','onion','carrot', 'garlic','butter','black pepper', 'cinnamon','coconut milk'],
-                                '1. Grill the butter squash, onion, carrot and garlic in the oven until '
-                                'they get soft and golden on top. \n' 
-                                '2. Put all in blender with butter and coconut milk. Blend them till they become puree. '
-                                'Then move the content into a pot. Add coconut milk. Simmer for 5 mintues.')
+butternut_sqaush_soup = FoodInfo(
+    'butternut squash soup', 45, True, 'soup', 'North African', 
+    ['butter squash','onion','carrot', 'garlic','butter','black pepper', 'cinnamon','coconut milk'],
+    '1. Grill the butter squash, onion, carrot and garlic in the oven until '
+    'they get soft and golden on top. \n' 
+    '2. Put all in blender with butter and coconut milk. Blend them till they become puree. '
+    'Then move the content into a pot. Add coconut milk. Simmer for 5 mintues.')
 
-shirazi_salad = FoodInfo('shirazi salad', 5, True, 'salad', 'Iranian',
-                        ['cucumber', 'tomato', 'onion', 'lemon juice'],
-                        '1. dice cucumbers, tomatoes and onions. \n'
-                        '2. put all into a bowl. \n' 
-                        '3. pour lemon juice and add salt. \n'
-                        '4. Mixed them thoroughly.')
+shirazi_salad = FoodInfo(
+    'shirazi salad', 5, True, 'salad', 'Iranian',
+    ['cucumber', 'tomato', 'onion', 'lemon juice'],
+    '1. dice cucumbers, tomatoes and onions. \n'
+    '2. put all into a bowl. \n' 
+    '3. pour lemon juice and add salt. \n'
+    '4. Mixed them thoroughly.')
 
-homemade_beef_sausage = FoodInfo('Home-made Beef Sausage', 60, False, 'deli', 'All',
-                                ['sausage casing', 'regular ground beef','garlic','corriander seeds',
-                                'black pepper seeds','fennel seed','paprika'],
-                                '1. In a blender, blend corriander seeds, black pepper seeds, '
-                                'fennel seeds and garlic to make the seasonings \n'
-                                '2. In a bowl, mix ground beef with the seasoning \n' 
-                                '3. Add all the content to a sausage stuffer. Put the casing on the stuffer funnel. '
-                                'Rotate the stuffer\'s handle (or turn it on) to make your yummy sausages!')
+homemade_beef_sausage = FoodInfo(
+    'Home-made Beef Sausage', 60, False, 'deli', 'All',
+    ['sausage casing', 'regular ground beef','garlic','corriander seeds',
+    'black pepper seeds','fennel seed','paprika'],
+    '1. In a blender, blend corriander seeds, black pepper seeds, '
+    'fennel seeds and garlic to make the seasonings \n'
+    '2. In a bowl, mix ground beef with the seasoning \n' 
+    '3. Add all the content to a sausage stuffer. Put the casing on the stuffer funnel. '
+    'Rotate the stuffer\'s handle (or turn it on) to make your yummy sausages!')
 
 foods = [butternut_sqaush_soup, shirazi_salad, homemade_beef_sausage]
 
